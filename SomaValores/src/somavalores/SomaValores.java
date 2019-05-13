c4bb2231c4f75c6fc1c94e21fd33c1a39450856c
Abençoado por Deus <Abenoado por Deus@DESKTOP-APBL8FE>
2019-05-13 11:01:08 -0300

    Commit inicial do projeto

---
+++ SomaValores/src/somavalores/SomaValores.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package somavalores;
+
+/**
+ *
+ * @author Abençoado por Deus
+ */
+public class SomaValores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor1 = 10;
+    }
+    
+    public static int Soma(int v1, int v2){
+        return v1+v2;
+    }
+    
+}
